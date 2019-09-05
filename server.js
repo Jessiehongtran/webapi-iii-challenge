2bf4d1caeb581d7e2baae76b50b0247c417132ba
Your Name <you@example.com>
2019-09-05 08:36:11 -0400

    test function logger

--- server.js
+++ server.js
@@ -6,10 +6,12 @@ server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`)
 });
 
+server.use(express.json())
+
 //custom middleware
+server.use(function logger(req, res, next) {
+  res.status(404).send("Ain't nobody got time for dat!")
+});
 
-function logger(req, res, next) {
-
-};
 
 module.exports = server;
