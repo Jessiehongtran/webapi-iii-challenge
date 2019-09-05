766f99eef51e7166ea07c27981d8b06bc76e88bb
Your Name <you@example.com>
2019-09-05 09:08:35 -0400

    tested to see how Express Middleware work

--- server.js
+++ server.js
@@ -9,9 +9,38 @@ server.get('/', (req, res) => {
 server.use(express.json())
 
 //custom middleware
-server.use(function logger(req, res, next) {
-  res.status(404).send("Ain't nobody got time for dat!")
-});
+function logger(req, res, next) {
+  console.log(
+    `[${new Date().toISOString()}] ${req.method} to ${req.url} from ${req.get('Origin')}`
+  );
+  next();
+};
+
+function atGate(req,res,next) {
+  console.log('At the gate, about to be eaten');
+  next();
+}
+
+function auth(req,res,next) {
+  if(req.url === '/melon'){
+    next()
+  } else {
+    res.send('You shall not pass')
+  }
+}
+
+server.get('/melon', (req,res) => {
+  console.log('Gate opening...');
+  console.log('Inside and safe');
+  res.send('Welcome Traveler')
+})
+
+server.get('/haha', (req,res) => {
+  res.send("Let's discover")
+})
 
 
+server.use(logger)
+server.use(atGate)
+server.use(auth)
 module.exports = server;
